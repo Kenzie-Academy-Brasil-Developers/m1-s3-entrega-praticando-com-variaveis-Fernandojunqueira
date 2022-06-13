f263d5a53aba88fc3128802a97a0bac30bc6a425
Fernandojunqueira <fernandojunqueira12@gmail.com>
2022-06-13 16:30:48 -0300

    Entrega Javascript 1

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Fernando";
+let sobrenome = "Junqueira";
+let anoNascimento = 1995;
+let anoAtual = 2022;
+let idade = anoAtual-anoNascimento
+let cidade = "Florianópolis";
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${idade} e moro em ${cidade}` )
